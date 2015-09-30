0ac1ac05eec02a84e0994e0a3c0a048f323f5f56
Vu Dao <tuanvu.dao@gmail.com>
2015-09-30 09:57:56 +0700

    Initial Commit

---
+++ Example Contact/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example Contact
+//
+//  Created by Vu Dao on 9/30/15.
+//  Copyright © 2015 Vu Dao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
